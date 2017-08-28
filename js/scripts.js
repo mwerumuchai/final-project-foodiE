03a77d8d6a13d081c81eac6a649fddfce4f1623e
mwerumuchai <mwerumuchai@gmail.com>
2017-08-28 20:42:29 +0300

    input field

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,35 @@ function showSlides() {
     slides[slideIndex-1].style.display = "block";
     setTimeout(showSlides, 4000); // Change image every 4 seconds
 }
+
+(function ($) {
+  jQuery.expr[':'].Contains = function(a,i,m){
+      return (a.textContent || a.innerText || "").toUpperCase().indexOf(m[3].toUpperCase())>=0;
+  };
+
+//input field to the header
+  function listFilter(header, list) {                   //create the filter form and append it to the header element
+    var form = $("<form>").attr({"class":"filterform","action":"#"}),
+        input = $("<input>").attr({"class":"filterinput","type":"text"});
+    $(form).append(input).appendTo(header);     //appendTo() function add the form and input field to the header tag
+
+    $(input)
+      .change( function () {
+        var filter = $(this).val();
+        if(filter) {
+          $(list).find(".recipe:not(:Contains(" + filter + "))").parent().slideUp();
+          $(list).find(".recipe:Contains(" + filter + ")").parent().slideDown();
+        } else {
+          $(list).find(".entry").slideDown();
+        }
+        return false;
+      })
+    .keyup( function () {
+        $(this).change();
+    });
+  }
+
+  $(function () {
+    listFilter($("#header"), $("#list"));
+  });
+}(jQuery));
